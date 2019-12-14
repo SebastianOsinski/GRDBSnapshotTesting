ca8a077171fc0485f0f9d46fae8517063ef9e1d8
Sebastian Osiński <seb.osinski@gmail.com>
2019-12-14 17:28:45 +0100

    Refactor tests

---
+++ GRDBSnapshotTestingTests/GRDBSnapshotTestingTests.swift
@@ -0,0 +1,133 @@
+//
+//  GRDBSnapshotTestingTests.swift
+//  GRDBSnapshotTestingTests
+//
+//  Created by Sebastian Osiński on 11/12/2019.
+//  Copyright © 2019 Sebastian Osiński. All rights reserved.
+//
+
+import XCTest
+import GRDB
+import SnapshotTesting
+import GRDBSnapshotTesting
+
+class GRDBSnapshotTestingTests: XCTestCase {
+    private let db = DatabaseQueue()
+    
+    private let record = false
+    
+    func testOneTable() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testTwoTables() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testOneIndex() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.BookAuthorIdIndex.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testTwoIndexes() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.BookAuthorIdIndex.self,
+            Migrations.AuthorCountryIndex.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testOneTrigger() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.AuthorUpdateTrigger.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testTwoTriggers() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.AuthorUpdateTrigger.self,
+            Migrations.BookUpdateTrigger.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testOneView() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.AuthorsPerCountryView.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testTwoViews() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.AuthorsPerCountryView.self,
+            Migrations.BooksPerAuthorView.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testData() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.Data.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    func testAll() throws {
+        try applyMigrations(db: db, migrations: [
+            Migrations.AuthorTable.self,
+            Migrations.BookTable.self,
+            Migrations.BookAuthorIdIndex.self,
+            Migrations.AuthorCountryIndex.self,
+            Migrations.BookUpdateTrigger.self,
+            Migrations.AuthorsPerCountryView.self,
+            Migrations.BooksPerAuthorView.self,
+            Migrations.Data.self
+        ])
+        
+        assertSnapshot(matching: db, as: .dbDump, record: record)
+    }
+    
+    private func applyMigrations(db: DatabaseQueue, migrations: [DBMigration.Type]) throws {
+        var migrator = DatabaseMigrator()
+        
+        for migration in migrations {
+            migrator.registerMigration(migration.name, migrate: migration.migrate)
+        }
+        
+        try migrator.migrate(db)
+    }
+}
---
+++ GRDBSnapshotTestingTests/Migrations.swift
@@ -0,0 +1,126 @@
+//
+//  Migrations.swift
+//  GRDBSnapshotTestingTests
+//
+//  Created by Sebastian Osiński on 12/12/2019.
+//  Copyright © 2019 Sebastian Osiński. All rights reserved.
+//
+
+import GRDB
+
+protocol DBMigration {
+    static var name: String { get }
+    
+    static func migrate(db: Database) throws
+}
+
+extension DBMigration {
+    static var name: String {
+        String(describing: self)
+    }
+}
+
+enum Migrations {
+    enum AuthorTable: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.create(table: "author") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("name", .text).notNull()
+                t.column("country", .text).notNull()
+                t.column("lastUpdate", .datetime)
+            }
+        }
+    }
+    
+    enum BookTable: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.create(table: "book") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("title", .text).notNull()
+                t.column("authorId", .integer).notNull().references("author", onDelete: .cascade)
+                t.column("lastUpdate", .datetime)
+            }
+        }
+    }
+    
+    enum BookAuthorIdIndex: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.create(index: "book_authorId", on: "book", columns: ["authorId"])
+        }
+    }
+    
+    enum AuthorCountryIndex: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.create(index: "author_country", on: "author", columns: ["country"])
+        }
+    }
+    
+    enum AuthorUpdateTrigger: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.execute(sql: """
+                CREATE TRIGGER author_update
+                    AFTER UPDATE ON author
+                    WHEN NEW.lastUpdate = OLD.lastUpdate
+                    BEGIN
+                        UPDATE place SET lastUpdate = CURRENT_TIMESTAMP WHERE id = NEW.id;
+                    END
+            """)
+        }
+    }
+    
+    enum BookUpdateTrigger: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.execute(sql: """
+                CREATE TRIGGER book_update
+                    AFTER UPDATE ON book
+                    WHEN NEW.lastUpdate = OLD.lastUpdate
+                    BEGIN
+                        UPDATE place SET lastUpdate = CURRENT_TIMESTAMP WHERE id = NEW.id;
+                    END
+            """)
+        }
+    }
+    
+    enum AuthorsPerCountryView: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.execute(sql: """
+                CREATE VIEW authorsPerCountry
+                AS
+                SELECT country, COUNT(*) as authorsCount
+                FROM author
+                GROUP BY country
+                ORDER BY authorsCount DESC
+            """)
+        }
+    }
+    
+    enum BooksPerAuthorView: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.execute(sql: """
+                CREATE VIEW booksPerAuthor
+                AS
+                SELECT author.name, count(book.id) as booksCount
+                FROM author
+                LEFT JOIN book ON author.id = book.authorId
+                GROUP BY author.name
+                ORDER BY booksCount DESC
+            """)
+        }
+    }
+    
+    enum Data: DBMigration {
+        static func migrate(db: Database) throws {
+            try db.execute(sql: """
+            INSERT INTO author VALUES (1, 'J.K. Rowling', 'UK', NULL);
+            INSERT INTO author VALUES (2, 'J.R.R. Tolkien', 'UK', NULL);
+            
+            INSERT INTO book VALUES (1, 'Harry Potter and the Philosopher''s Stone', 1, NULL);
+            INSERT INTO book VALUES (2, 'Harry Potter and the Chamber of Secrets', 1, NULL);
+            INSERT INTO book VALUES (3, 'Harry Potter and the Prisoner of Azkaban', 1, NULL);
+
+            INSERT INTO book VALUES (4, 'The Hobbit', 2, NULL);
+            INSERT INTO book VALUES (5, 'The Fellowship of the Ring', 2, NULL);
+            """)
+        }
+    }
+}
