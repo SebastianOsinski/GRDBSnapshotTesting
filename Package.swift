94a5b512d61fecbee2227d500bae8c2d6edb6c57
Gwendal Roué <gr@pierlis.com>
2019-12-20 09:49:17 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GRDBSnapshotTesting",
+    platforms: [
+      .iOS(.v10),
+      .macOS(.v10_10),
+      .tvOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "GRDBSnapshotTesting",
+            targets: ["GRDBSnapshotTesting"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .upToNextMajor(from: "1.5.0")),
+    ],
+    targets: [
+        .target(
+            name: "GRDBSnapshotTesting",
+            dependencies: ["GRDB", "SnapshotTesting"],
+            path: "GRDBSnapshotTesting/Sources")
+    ]
+)
