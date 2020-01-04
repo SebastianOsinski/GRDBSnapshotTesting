8a66094485419886b36356da018b29654e40cbf2
Sebastian Osiński <seb.osinski@gmail.com>
2020-01-04 16:34:56 +0100

    Order objects by name in snapshot

--- GRDBSnapshotTesting/Sources/Dump/SQLiteMaster.swift
+++ GRDBSnapshotTesting/Sources/Dump/SQLiteMaster.swift
@@ -27,6 +27,7 @@ extension SQLiteMaster {
     private enum Columns {
         static let type = Column("type")
         static let sql = Column("sql")
+        static let name = Column("name")
     }
     
     enum Requests {
@@ -35,18 +36,22 @@ extension SQLiteMaster {
             .filter(Columns.type == RecordType.table.rawValue)
             .filter(literal: "name NOT LIKE 'sqlite\\_%' ESCAPE '\\'")
             .filter(literal: "name NOT LIKE 'grdb\\_%' ESCAPE '\\'")
+            .order(Columns.name.asc)
         
         static let indexes = SQLiteMaster
             .all()
             .filter(Columns.type == RecordType.index.rawValue)
             .filter(Columns.sql != nil) // All user-created indexes have sql, only automatic have null sql
+            .order(Columns.name.asc)
         
         static let triggers = SQLiteMaster
             .all()
             .filter(Columns.type == RecordType.trigger.rawValue)
+            .order(Columns.name.asc)
         
         static let views = SQLiteMaster
             .all()
             .filter(Columns.type == RecordType.view.rawValue)
+            .order(Columns.name.asc)
     }
 }
